e0b6c46a9b4d8b8b422016ca8559366d7ca668b3
Zac Stewart <zgstewart@gmail.com>
2015-02-15 14:44:42 -0500

    Initial submission

---
+++ classifier.py
@@ -0,0 +1,68 @@
+from pandas import DataFrame
+from skimage.io import imread_collection
+from sklearn import metrics
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from transformers import NormalizeImages, ResampleImages
+import numpy as np
+import os
+from IPython.core.debugger import Tracer
+tracer = Tracer()
+
+folders = os.listdir('data/train')
+train = np.empty((0, 2))
+
+for folder in folders:
+    examples = [[folder, os.path.join('data/train', folder, example)] for
+                example in os.listdir(os.path.join('data/train', folder))]
+    train = np.concatenate((train, examples), axis=0)
+
+train_y = train[:, 0]
+train_x = train[:, 1]
+classes = list(set(train_y))
+classes.sort()
+print("Reading train images...")
+train_x = np.array(imread_collection(train_x, conserve_memory=False))
+
+kf = StratifiedKFold(train_y, n_folds=3, shuffle=True)
+
+pipeline = Pipeline([
+    ('normalize_imgs', NormalizeImages(capture_percentage=.8)),
+    ('resized_imgs', ResampleImages(32)),
+    ('classifier', SVC(probability=True, verbose=True))
+])
+
+print("Cross validating...")
+scores = []
+for fold, (construct_idx, validate_idx) in enumerate(kf):
+    print("Fold {}...".format(fold + 1))
+    pipeline.fit(train_x[construct_idx], train_y[construct_idx])
+    predictions = pipeline.predict_proba(train_x[validate_idx])
+    score = metrics.log_loss(train_y[validate_idx], predictions)
+    scores.append(score)
+
+print("Mean score:", np.mean(scores))
+print("Score std:", np.std(scores))
+print("Raw scores:", scores)
+
+print("Training...")
+
+pipeline.fit(train_x, train_y)
+
+print("Loading test images...")
+
+images = np.array(os.listdir('data/test')).reshape((-1, 1))
+test = np.array([os.path.join('data/test', filename)
+                for filename in os.listdir('data/test')])
+test_x = np.array(imread_collection(test, conserve_memory=False))
+
+print("Predicting...")
+
+probabilities = pipeline.predict_proba(test_x)
+submission = DataFrame(data=probabilities, columns=classes)
+submission['image'] = images
+data = np.append(images, probabilities, axis=1)
+submission.to_csv('submission.csv', index=False)
+
+tracer()
---
+++ transformers.py
@@ -0,0 +1,82 @@
+import pdb
+from skimage.io import imread_collection
+from sklearn.base import TransformerMixin
+from skimage.transform import resize
+import math
+import numpy as np
+
+
+class NormalizeImages(TransformerMixin):
+
+    def __init__(self, capture_percentage=0.8):
+        self.capture_percentage = capture_percentage
+        self.max_width = 0
+        self.max_height = 0
+
+    def fit(self, x, y, **kwargs):
+        sizes = []
+        for image in x:
+            sizes.append(max(image.shape))
+        sizes = np.array(sizes)
+
+        capture_count = len(x) * self.capture_percentage
+
+        for i in range(max(sizes)):
+            self.size = i
+            if (sizes < i).sum() >= capture_count:
+                break
+
+        return self
+
+    def transform(self, x):
+        output = np.full((len(x), self.size, self.size), 0)
+        for i, image in enumerate(x):
+            (image_height, image_width) = image.shape
+            diff_y = math.floor(float(self.size - image_height) / 2.0)
+            diff_x = math.floor(float(self.size - image_width) / 2.0)
+            height = min(self.size, image_height)
+            width = min(self.size, image_width)
+
+            if diff_y >= 0:
+                output_offset_y = diff_y
+                image_offset_y = 0
+            else:
+                output_offset_y = 0
+                image_offset_y = -diff_y
+
+            if diff_x >= 0:
+                output_offset_x = diff_x
+                image_offset_x = 0
+            else:
+                output_offset_x = 0
+                image_offset_x = -diff_x
+
+            crop = image[
+                image_offset_y:(image_offset_y+height),
+                image_offset_x:(image_offset_x+width)
+            ]
+            output[
+                i,
+                output_offset_y:(output_offset_y+height),
+                output_offset_x:(output_offset_x+width)
+            ] = 255.0 - crop
+        return output.reshape((len(x), -1)) / 255
+
+
+class ResampleImages(TransformerMixin):
+
+    def __init__(self, height, width=None):
+        self.height = height
+        self.width = width or height
+
+    def fit(self, x, y, **kwargs):
+        return self
+
+    def transform(self, x, **kwargs):
+        n, m = x.shape
+        size = np.sqrt(m)
+        output = np.full((len(x), self.height, self.width), 255)
+        for i, image in enumerate(x):
+            image = image.reshape((size, size))
+            output[i] = resize(image, (self.height, self.width))
+        return output.reshape((len(x), -1))
